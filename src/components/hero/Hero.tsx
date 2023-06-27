42264a2a2314e25c78e3f5d4984a37b427f718fa
unknown <khanasfand174@gmail.com>
2023-06-27 13:44:15 +0500

    fix UI issues

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,7 +2,7 @@ import Form from "./Form"
 
 const Hero = () => {
   return (
-    <div className="w-full h-[120vh] md:h-screen parallex bg-[url('../../public/header-image.png')] bg-cover bg-no-repeat bg-center">
+    <div className="w-full py-20 parallex bg-[url('../../public/header-image.png')] bg-cover bg-no-repeat bg-center">
         <div className='grid place-items-center justify-center h-[100%]'>
             <Form/>
         </div>

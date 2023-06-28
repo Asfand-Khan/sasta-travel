5380c95a22cc64d6080aac635edfffa0e24a6409
unknown <khanasfand174@gmail.com>
2023-06-28 10:53:07 +0500

    fixed multi form responsive issues

--- src/components/hero/Multi.tsx
+++ src/components/hero/Multi.tsx
@@ -11,81 +11,81 @@ const Multi = () => {
 
   return (
     <div>
-      <div className="flex flex-col md:flex-row gap-2 justify-center mb-2">
-        <h4 className="font-semibold text-mainColor text-xl">Flight 1</h4>
-        <div className="flex flex-col gap-3">
-          <Label htmlFor="flyingFrom" className="font-normal">
-            Flying From
-          </Label>
-          <Input
-            type="text"
-            placeholder="Flying From"
-            id="flyingFrom"
-            className="py-7 px-4 w-full md:w-[20vw]"
-          />
+        {/*********** Flight 1 Form **********/}
+        <div className="flex flex-col gap-2 justify-center mb-2 md:items-center">
+          <div className="flex flex-col gap-3">
+            <h4 className=" font-semibold text-mainColor text-xl">Flight 1</h4>
+            <Label htmlFor="flyingFrom" className="font-normal">
+              Flying From
+            </Label>
+            <Input
+              type="text"
+              placeholder="Flying From"
+              id="flyingFrom"
+              className="py-7 px-4 w-full md:w-[20vw]"
+            />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="flyingTo" className="font-normal">
+              Flying To
+            </Label>
+            <Input
+              type="text"
+              placeholder="Flying To"
+              id="flyingTo"
+              className="py-7 px-4 w-full md:w-[20vw]"
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-3">
-          <Label htmlFor="flyingTo" className="font-normal">
-            Flying To
-          </Label>
-          <Input
-            type="text"
-            placeholder="Flying To"
-            id="flyingTo"
-            className="py-7 px-4 w-full md:w-[20vw]"
-          />
+        <div className="flex flex-col gap-2 justify-center mb-2 md:items-center">
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="datePicker" className="font-normal">
+              Pick A Date
+            </Label>
+            <DatePicker date={date} setDate={setDate} />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col md:flex-row gap-2 justify-center mb-2">
-        <div className="flex flex-col gap-3">
-          <Label htmlFor="datePicker" className="font-normal">
-            Pick A Date
-          </Label>
-          <DatePicker date={date} setDate={setDate} />
-        </div>
-      </div>
 
         {/************************** Flight 2 Form *****************/}
 
-        <div className="flex flex-col md:flex-row gap-2 justify-center mb-2">
-        <h4 className="font-semibold text-mainColor text-xl">Flight 2</h4>
-        <div className="flex flex-col gap-3">
-          <Label htmlFor="flyingFrom" className="font-normal">
-            Flying From
-          </Label>
-          <Input
-            type="text"
-            placeholder="Flying From"
-            id="flyingFrom"
-            className="py-7 px-4 w-full md:w-[20vw]"
-          />
+        <div className="flex flex-col gap-2 justify-center mb-2 md:items-center">
+          <div className="flex flex-col gap-3">
+            <h4 className="font-semibold text-mainColor text-xl">Flight 2</h4>
+            <Label htmlFor="flyingFrom" className="font-normal">
+              Flying From
+            </Label>
+            <Input
+              type="text"
+              placeholder="Flying From"
+              id="flyingFrom"
+              className="py-7 px-4 w-full md:w-[20vw]"
+            />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="flyingTo" className="font-normal">
+              Flying To
+            </Label>
+            <Input
+              type="text"
+              placeholder="Flying To"
+              id="flyingTo"
+              className="py-7 px-4 w-full md:w-[20vw]"
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-3">
-          <Label htmlFor="flyingTo" className="font-normal">
-            Flying To
-          </Label>
-          <Input
-            type="text"
-            placeholder="Flying To"
-            id="flyingTo"
-            className="py-7 px-4 w-full md:w-[20vw]"
-          />
+        <div className="flex flex-col gap-2 justify-center mb-2 md:items-center">
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="datePicker" className="font-normal">
+              Pick A Date
+            </Label>
+            <DatePicker date={date} setDate={setDate} />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col md:flex-row gap-2 justify-center mb-2">
-        <div className="flex flex-col gap-3">
-          <Label htmlFor="datePicker" className="font-normal">
-            Pick A Date
-          </Label>
-          <DatePicker date={date} setDate={setDate} />
-        </div>
-      </div>
-
       {/********************** Travellers Option *******************/}
 
       <div className="flex flex-col md:flex-row gap-2 justify-center mb-2">
-      <h4 className="font-semibold text-mainColor text-xl">Travellers</h4>
         <div className="flex flex-col gap-3">
+          <h4 className="font-semibold text-mainColor text-xl">Travellers</h4>
           <Label htmlFor="typeAndPerson" className="font-normal">
             No. Of Person & Type
           </Label>

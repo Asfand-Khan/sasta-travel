20f7d77ecfd5dcf211d59b4884c914fa692b09a0
unknown <khanasfand174@gmail.com>
2023-06-28 11:20:11 +0500

    storing one way form values to the states

--- src/components/hero/OneWay.tsx
+++ src/components/hero/OneWay.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import DatePicker from "../ui/datepicker";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
 const OneWay = () => {
+  const [flyingFrom ,setFlyingFrom] = useState<string>("");
+  const [flyingTo ,setFlyingTo] = useState<string>("");
   const [date, setDate] = useState<Date>();
+  const [noOfAdult,setNoOfAdult] = useState<number>(1);
+  const [noOfChildren,setNoOfChildren] = useState<number>(0);
+  const [noOfInfant,setNoOfInfant] = useState<number>(0);
+  const [typeOfFlight,setTypeOfFlight] = useState<string>("Economy");
 
   return (
     <div>
       <div className="flex flex-col md:flex-row gap-2 justify-center mb-2">
-        <div className="flex flex-col gap-3">
+        <div className="relative flex flex-col gap-3">
           <Label htmlFor="flyingFrom" className="font-normal">
             Flying From
           </Label>
@@ -21,6 +27,7 @@ const OneWay = () => {
             placeholder="Flying From"
             id="flyingFrom"
             className="py-7 px-4 w-full md:w-[20vw]"
+            value={flyingFrom}
           />
         </div>
         <div className="flex flex-col gap-3">
@@ -32,6 +39,7 @@ const OneWay = () => {
             placeholder="Flying To"
             id="flyingTo"
             className="py-7 px-4 w-full md:w-[20vw]"
+            value={flyingTo}
           />
         </div>
       </div>
@@ -51,6 +59,7 @@ const OneWay = () => {
             placeholder="No. of persons & Type"
             id="typeAndPerson"
             className="py-7 px-4 w-full md:w-[20vw]"
+            value={`${noOfAdult+noOfChildren+noOfInfant} Traveller(s), ${typeOfFlight}`}
           />
         </div>
       </div>
